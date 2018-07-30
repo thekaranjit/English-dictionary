92afe7c324221f605303f4efd979f8c6a1e7b97f
thekaranjit <thekaranjit@gmail.com>
2018-07-31 03:09:46 +0400

    Created a function

--- dictionary.py
+++ dictionary.py
@@ -1,2 +1,17 @@
-# A simple python dictionary
+# Python english dictionary
+
+import json
+
+data = json.load(open("data.json"))
+
+def translate(word):
+    print(data[word])
+    
+
+
+
+word = input("Please input the word to search the meaning: ")
+
+translate(word)
+#print(data["rain"])
 
