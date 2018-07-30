9defebed16af7098e14c94b867d07943beb361aa
thekaranjit <thekaranjit@gmail.com>
2018-07-31 03:45:51 +0400

    Ready to merge

--- dictionary.py
+++ dictionary.py
@@ -1,17 +1,29 @@
 # Python english dictionary
 
 import json
+import difflib
+from difflib import SequenceMatcher
+
+from difflib import get_close_matches
 
 data = json.load(open("data.json"))
 
 def translate(word):
-    print(data[word])
-    
+    word = word.lower()
+    if word in data:
+
+        return data[word]
+
+    else:
+        return "The word doesnt exist. Please Try Again"
 
 
 
-word = input("Please input the word to search the meaning: ")
 
-translate(word)
+
+
+word = input("Please input the wsexord to search the meaning: ")
+
+print(translate(word))
 #print(data["rain"])
 
